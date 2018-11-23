4ee496103bd52bc22507ea9954061b62a13a5d68
uche01 <urchboy995@gmail.com>
2018-11-23 14:50:34 +0100

    v1

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/fela-admin'));
+app.use(express.static(__dirname + '/dist/futo-e-learning-admin'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/fela-admin/index.html'));
+res.sendFile(path.join(__dirname+'/dist/futo-e-learning-admin/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
